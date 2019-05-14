c9df64caa83bc266e62aa741f20df25f4fa4a40e
yangshngce <1031595893@qq.com>
2019-05-15 03:21:05 +0800

    testysc

--- src/test/java/TripMode/TestDB.java
+++ src/test/java/TripMode/TestDB.java
@@ -1,13 +1,26 @@
 package TripMode;
 
-import TripMode.dao.IniDB;
+import TripMode.model.Crossing;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
+import java.util.List;
 
 public class TestDB {
-    public static void main(String[] args) throws Exception {
-        IniDB iniDB=new IniDB();
-        Connection connection=iniDB.getConnection();
+    public static void main(String[] args) throws IOException {
+        InputStream ab = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(ab);
+        SqlSession session = factory.openSession();
+        List<Crossing> list = session.selectList("test.selAll");
+        for(Crossing crossing:list){
+            System.out.print(crossing);
+        }
+        session.close();
     }
 
 }
