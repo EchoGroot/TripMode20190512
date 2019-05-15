e894ffdd98e12d7ef08a9ba37511606d67256b32
yangshngce <1031595893@qq.com>
2019-05-15 13:38:40 +0800

    testysc

--- src/test/java/TripMode/TestDB.java
+++ src/test/java/TripMode/TestDB.java
@@ -1,25 +1,34 @@
 package TripMode;
 
-import TripMode.model.Crossing;
+
+import TripMode.pojo.TheCrossing;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.SQLOutput;
 import java.util.List;
+import java.util.Scanner;
 
 public class TestDB {
     public static void main(String[] args) throws IOException {
+        String id;
         InputStream ab = Resources.getResourceAsStream("SqlMapConfig.xml");
         SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(ab);
         SqlSession session = factory.openSession();
-        List<Crossing> list = session.selectList("test.selAll");
-        for(Crossing crossing:list){
+        List<TheCrossing> list = session.selectList("test.selAll");
+        Scanner a = new Scanner(System.in);
+        id=a.nextLine();
+        //TheCrossing crossing =  session.selectOne("getLocation.getLocation",id);
+        for(TheCrossing crossing:list){
             System.out.print(crossing);
         }
+        //System.out.println(crossing.toString());
         session.close();
     }
 
